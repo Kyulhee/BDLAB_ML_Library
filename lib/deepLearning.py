531a0ba73df25d840ac2e5bad39fe9ee1c32d520
GooTec <21100037@handong.edu>
2018-06-29 17:28:29 +0900

    LATEST

--- lib/deepLearning.py
+++ lib/deepLearning.py
@@ -3,6 +3,24 @@ import numpy as np
 
 '''구분해서 사용할 수 있게 해야 하는 내용 activation function'''
 
+def get_activation_fn(input_tensor, activation_fn='relu'):
+    try :
+        if activation_fn == 'relu' :
+            return tf.nn.relu(input_tensor, 'relu')
+        elif activation_fn == 'sigmoid':
+            return tf.sigmoid(input_tensor, 'sigmoid')
+        elif activation_fn =='tanh':
+            return tf.tanh(input_tensor, 'tanh')
+        else:
+            raise TypeError
+    except TypeError as TE:
+        print("Wrong Activation Function ")
+        raise
+    except :
+        raise
+
+
+
 def dense(x, size, scope, activation_fn):
     '''
     :param x: 입력 텐서
@@ -26,10 +44,9 @@ def dense(x, size, scope, activation_fn):
     scope=None
     )
     '''
-
     return tf.contrib.layers.fully_connected(x, size, activation_fn=activation_fn,scope=scope )
 
-def dense_batch_relu(x, phase, size, scope):
+def dense_batch(x, phase, size, scope, activation_fn='relu'):
     '''
     :param x: 입력 텐서
     :param phase: 현재 텐서플로우가 학습 중인지 테스트 중인지를 구분한다 phase가 True이면 학습 False이면 테스트
@@ -40,10 +57,10 @@ def dense_batch_relu(x, phase, size, scope):
     with tf.variable_scope(scope):
         h1 = tf.contrib.layers.fully_connected(x, size,activation_fn=None,scope='dense')
         h2 = tf.contrib.layers.batch_norm(h1, center=True, scale=True, is_training=phase,scope='bn')
-        return tf.nn.relu(h2, 'relu')
+        return get_activation_fn(h2, activation_fn)
 
 
-def dense_dropout_relu(x,phase, size, scope, rate=0.5):
+def dense_dropout(x,phase, size, scope, rate=0.5, activation_fn='relu'):
     '''
     :param x: 입력 텐서
     :param phase: 현재 텐서플로우가 학습 중인지 테스트 중인지를 구분한다 phase가 True이면 학습 False이면 테스트
@@ -55,11 +72,11 @@ def dense_dropout_relu(x,phase, size, scope, rate=0.5):
     with tf.variable_scope(scope):
         h1 = tf.contrib.layers.fully_connected(x, size, activation_fn=None, scope='dense')
         h2 = tf.contrib.layers.dropout(h1, keep_prob=rate , is_training = phase ,scope='do')
-        return tf.nn.relu(h2, 'relu')
+        return get_activation_fn(h2, activation_fn)
 
 
 
-def dense_batch_dropout_relu(x,phase, size, scope, rate=0.5):
+def dense_batch_dropout(x,phase, size, scope, rate=0.5, activation_fn='relu'):
     '''
     :param x: 입력 텐서
     :param phase: 현재 텐서플로우가 학습 중인지 테스트 중인지를 구분한다 phase가 True이면 학습 False이면 테스트
@@ -72,10 +89,10 @@ def dense_batch_dropout_relu(x,phase, size, scope, rate=0.5):
         h1 = tf.contrib.layers.fully_connected(x, size, activation_fn=None, scope='dense')
         h2 = tf.contrib.layers.batch_norm(h1, center=True, scale=True, is_training=phase,scope='bn')
         h3 = tf.contrib.layers.dropout(h2, keep_prob=rate , is_training = phase ,scope='do')
-        return tf.nn.relu(h3, 'relu')
+        return get_activation_fn(h3, activation_fn)
 
 
-def set_model_dropout(x, y, nodes , learning_rate):
+def set_model_dropout(x, y, nodes , learning_rate, activation_fn ='relu'):
     '''
     :param x: 입력 데이터
     :param y: 기댓값
@@ -97,9 +114,9 @@ def set_model_dropout(x, y, nodes , learning_rate):
 
     for i in range(len(nodes)):
         if i == 0:
-            layers.append(dense_dropout_relu(X, phase, nodes[i], 'layer'+str(i+1), keep_prob))
+            layers.append(dense_dropout(X, phase, nodes[i], 'layer'+str(i+1), keep_prob, activation_fn))
         else:
-            layers.append(dense_dropout_relu(layers[i-1], phase, nodes[i], 'layer'+str(i+1), keep_prob))
+            layers.append(dense_dropout(layers[i-1], phase, nodes[i], 'layer'+str(i+1), keep_prob, activation_fn))
 
     logits = dense(layers[-1], len(y[0]), 'logits', None)
     hypothesis = tf.nn.softmax(logits)
@@ -114,7 +131,7 @@ def set_model_dropout(x, y, nodes , learning_rate):
     return X , Y , layers, logits  ,phase, hypothesis, cost , train, predicted , correct_prediction , accuracy , keep_prob
 
 
-def set_model_BN(x, y, nodes, learning_rate) :
+def set_model_BN(x, y, nodes, learning_rate, activation_fn='relu') :
     tf.reset_default_graph()
 
     np.random.seed(777)
@@ -127,9 +144,9 @@ def set_model_BN(x, y, nodes, learning_rate) :
 
     for i in range(len(nodes)):
         if i == 0:
-            layers.append(dense_batch_relu(X, phase, nodes[i], 'layer'+str(i+1)))
+            layers.append(dense_batch(X, phase, nodes[i], 'layer'+str(i+1), activation_fn))
         else :
-            layers.append(dense_batch_relu(layers[i-1], phase, nodes[i], 'layer'+str(i+1)))
+            layers.append(dense_batch(layers[i-1], phase, nodes[i], 'layer'+str(i+1), activation_fn))
     # h1 = dense_batch_relu(X, phase, nodes[0], 'layer1')
     # h2 = dense_batch_relu(h1, phase, nodes[1],'layer2')
     # h3 = dense_batch_relu(h2, phase, nodes[2],'layer3')
@@ -153,7 +170,7 @@ def set_model_BN(x, y, nodes, learning_rate) :
     return X , Y , layers, logits, phase  , hypothesis, cost , train, predicted , correct_prediction , accuracy , keep_prob
 
 
-def set_model_batch_dropout(x, y, nodes , learning_rate):
+def set_model_batch_dropout(x, y, nodes , learning_rate, activation_fn='relu'):
     '''
     :param x: 입력 데이터
     :param y: 기댓값
@@ -175,9 +192,9 @@ def set_model_batch_dropout(x, y, nodes , learning_rate):
 
     for i in range(len(nodes)):
         if i == 0:
-            layers.append(dense_batch_dropout_relu(X, phase, nodes[i], 'layer'+str(i+1), keep_prob))
+            layers.append(dense_batch_dropout(X, phase, nodes[i], 'layer'+str(i+1), keep_prob, activation_fn))
         else:
-            layers.append(dense_batch_dropout_relu(layers[i-1], phase, nodes[i], 'layer'+str(i+1), keep_prob))
+            layers.append(dense_batch_dropout(layers[i-1], phase, nodes[i], 'layer'+str(i+1), keep_prob, activation_fn))
 
     logits = dense(layers[-1], len(y[0]), 'logits', None)
     hypothesis = tf.nn.softmax(logits)
@@ -202,6 +219,7 @@ def set_model_basic(x, y, nodes , learning_rate, activation_fn):
     np.random.seed(777)
     keep_prob = tf.placeholder(tf.float32) #0.7 -> 70% 켜진 채로 30% 꺼진 채로
     layers = []
+    phase = tf.placeholder(tf.bool, name='phase')
 
     X = tf.placeholder(tf.float32, [None, len(x[0])])
     Y = tf.placeholder(tf.float32, [None, len(y[0])])
@@ -240,10 +258,10 @@ def set_model_basic(x, y, nodes , learning_rate, activation_fn):
     correct_prediction = tf.equal( predicted, tf.argmax( Y, 1))
     accuracy = tf.reduce_mean(tf.cast( correct_prediction, dtype=tf.float32))
 
-    return X , Y , layers, logits  , hypothesis, cost , train, predicted , correct_prediction , accuracy , keep_prob
+    return X , Y , layers, logits  , phase,  hypothesis, cost , train, predicted , correct_prediction , accuracy , keep_prob
 
 
-def set_model_by_paramter(x, y, nodes, learning_rate, drop_out, batch_norm, activation_fn):
+def set_model_by_paramter(x, y, nodes, learning_rate, drop_out=True, batch_norm=False, activation_fn='relu'):
     '''
     tf.nn.relu
     tf.nn.relu6
@@ -258,16 +276,30 @@ def set_model_by_paramter(x, y, nodes, learning_rate, drop_out, batch_norm, acti
     '''
     if activation_fn == 'relu' :
         if drop_out and batch_norm:
-            return set_model_batch_dropout(x,y,nodes,learning_rate)
+            return set_model_batch_dropout(x,y,nodes,learning_rate )
         elif drop_out :
             return set_model_dropout(x, y, nodes, learning_rate)
         elif batch_norm :
-            return set_model_BN(x,y,nodes,learning_rate)
+            return set_model_BN(x,y,nodes,learning_rate )
         else :
             return set_model_basic(x,y,nodes,learning_rate,'relu')
     elif activation_fn == 'sigmoid' :
-        return set_model_basic(x,y,nodes,learning_rate,'sigmoid')
+        if drop_out and batch_norm:
+            return set_model_batch_dropout(x,y,nodes,learning_rate, 'sigmoid')
+        elif drop_out :
+            return set_model_dropout(x, y, nodes, learning_rate, 'sigmoid')
+        elif batch_norm :
+            return set_model_BN(x,y,nodes,learning_rate, 'sigmoid')
+        else :
+            return set_model_basic(x,y,nodes,learning_rate,'sigmoid')
     elif activation_fn == 'tanh' :
-        return set_model_basic(x,y,nodes,learning_rate,'tanh')
+        if drop_out and batch_norm:
+            return set_model_batch_dropout(x,y,nodes,learning_rate, 'tanh')
+        elif drop_out :
+            return set_model_dropout(x, y, nodes, learning_rate, 'tanh')
+        elif batch_norm :
+            return set_model_BN(x,y,nodes,learning_rate, 'tanh')
+        else :
+            return set_model_basic(x,y,nodes,learning_rate,'tanh')
     else :
         return set_model_dropout(x, y, nodes, learning_rate)
