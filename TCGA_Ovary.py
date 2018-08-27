039281ae04c130d29608883fd3b72779b501c34d
Kyulhee <hgh97617@gmail.com>
2018-08-27 17:53:33 +0900

    hyunjung

--- TCGA_Ovary.py
+++ TCGA_Ovary.py
@@ -46,8 +46,6 @@ for file_type in file_types:
     train_y, test_y = dp.train_test(ydata_five, 0)
     train_y = dp.one_hot_encoder(train_y)
     test_y = dp.one_hot_encoder(test_y)
-    val_x, test_x = dp.test_validation(test_x)
-    val_y, test_y = dp.test_validation(test_y)
 
     #set hyperparameters - node, learning rate, batch size
     #'''
@@ -73,13 +71,10 @@ for file_type in file_types:
     batch_size_box = []
     tr_accuracy_box = []
     ts_accuracy_box = []
-    val_accuracy_box = []
     tr_sensitivity_box = []
     ts_sensitivity_box = []
-    val_sensitivity_box = []
     tr_specificity_box = []
     ts_specificity_box = []
-    val_specificity_box = []
     index = []
     
     tr_TPs = []
@@ -90,10 +85,6 @@ for file_type in file_types:
     ts_TNs = []
     ts_FPs = []
     ts_FNs = []
-    val_TPs = []
-    val_TNs = []
-    val_FPs = []
-    val_FNs = []
 
     for node in nodes:
         for learning_rate in learning_rates:
@@ -103,7 +94,6 @@ for file_type in file_types:
                 X, Y, layers, logits, phase, hypothesis, cost, train, predicted, correct_prediction, accuracy, keep_prob = dl.set_model_dropout(train_x, train_y, node , learning_rate)
 
                 best_train_acc = 0
-                best_val_acc = 0
                 best_test_acc = 0 
                 best_cost = float("inf")
                 save_path_dir ='../models/'
@@ -129,19 +119,16 @@ for file_type in file_types:
 
                         #feed_dict - place holder is just 'space', feed_dict means supply real data into place holder.
                         train_h, train_c, train_p, train_a = sess.run( [hypothesis, cost, predicted, accuracy], feed_dict={X: train_x, Y: train_y, keep_prob: 1 , phase:False})
-                        val_h, val_c, val_p, val_a = sess.run([hypothesis, cost, predicted, accuracy], feed_dict={X: val_x, Y: val_y, keep_prob: 1 , phase:False})
                         if step % 20 == 0 :
-                            print("train acc : ", train_a, "validation acc : ", val_a, "train_cost", train_c)
+                            print("train acc : ", train_a, "train_cost", train_c)
                         step += 1
                         
                         #condition 1: new best condition. 
-                        #calculated cost(val_c) is smaller than before's(best_cost), save present condition and initialize count.
-                        if best_cost > val_c :
+                        #calculated cost(train_c) is smaller than before's(best_cost), save present condition and initialize count.
+                        if best_cost > train_c :
                             best_train_acc = train_a
                             best_train_p = train_p
-                            best_val_acc = val_a
-                            best_val_p = val_p
-                            best_cost = val_c
+                            best_cost = train_c
                             count = 0
                             save_path = saver.save(sess, save_path_dir + 'model'+str(num)+'.ckpt')
 
@@ -155,7 +142,7 @@ for file_type in file_types:
                         else:
                             count += 1
 
-                    print("########## Training Accuracy : ", best_train_acc,  "Validation Accuracy : ", best_val_acc)
+                    print("########## Training Accuracy : ", best_train_acc)
 
                     saver.restore(sess, save_path)
 
@@ -166,13 +153,11 @@ for file_type in file_types:
 
                     model_num += 1
                     train_label, test_label = dp.train_test(ydata_five, 0)
-                    val_label, test_label = dp.test_validation_label(test_label)
 
                 with tf.Session() as sess:
                     tr_table_temp = tf.confusion_matrix(train_label, best_train_p, num_classes=2, dtype=tf.int64, name=None, weights=None)
-                    val_table_temp = tf.confusion_matrix(val_label, best_val_p, num_classes=2, dtype=tf.int64, name=None, weights=None)
                     ts_table_temp = tf.confusion_matrix(test_label, test_p, num_classes=2, dtype=tf.int64, name=None, weights=None)
-                    tr_table, val_table, ts_table = sess.run([tr_table_temp, val_table_temp, ts_table_temp])
+                    tr_table, ts_table = sess.run([tr_table_temp, ts_table_temp])
                 
                 # save contingency table
                 tr_TN, tr_FN, tr_FP, tr_TP = tr_table[0,0], tr_table[1,0], tr_table[0,1], tr_table[1,1]
@@ -186,13 +171,6 @@ for file_type in file_types:
                 ts_TNs.append(ts_TN)
                 ts_FPs.append(ts_FP)
                 ts_FNs.append(ts_FN)
-
-                val_TN, val_FN, val_FP, val_TP = val_table[0,0], val_table[1,0], val_table[0,1], val_table[1,1]
-                val_TPs.append(val_TP)
-                val_TNs.append(val_TN)
-                val_FPs.append(val_FP)
-                val_FNs.append(val_FN)
-     
                     
                 data_x, data_y = dp.divide_xy_test(raw_data, 'Platinum_Status', 1,-2)
                 ids = raw_data['patient']
@@ -223,19 +201,13 @@ for file_type in file_types:
                 ts_sensitivity_box.append(ts_TP/(ts_TP+ts_FN))
                 ts_specificity_box.append(ts_TN/(ts_TN+ts_FP))
                 
-                val_accuracy_box.append(best_val_acc)
-                val_sensitivity_box.append(val_TP/(val_TP+val_FN))
-                val_specificity_box.append(val_TN/(val_TN+val_FP))
-                
 
                 
     #train y, train 
-    df1 = df(data={'index': index, 'nodes': nodes_box, 'learning_rate': learning_rate_box, 'batch_sizes': batch_size_box, 'tr_accuracy':tr_accuracy_box, 'tr_sensitivity':tr_sensitivity_box, 'tr_specificity':tr_specificity_box, 'val_accuracy':val_accuracy_box, 'val_sensitivity':val_sensitivity_box, 'val_specificity':val_specificity_box, 'ts_accuracy': ts_accuracy_box, 'ts_sensitivity':ts_sensitivity_box, 'ts_specificity':ts_specificity_box })
+    df1 = df(data={'index': index, 'nodes': nodes_box, 'learning_rate': learning_rate_box, 'batch_sizes': batch_size_box, 'tr_accuracy':tr_accuracy_box, 'tr_sensitivity':tr_sensitivity_box, 'tr_specificity':tr_specificity_box, 'ts_accuracy': ts_accuracy_box, 'ts_sensitivity':ts_sensitivity_box, 'ts_specificity':ts_specificity_box })
     df1.to_csv("../result/OV_DNN_result_"+file_type+".csv", index=False, header=True, mode='w')
-    df2 = df(data={'index': index, 'nodes': nodes_box, 'learning_rate': learning_rate_box, 'batch_sizes': batch_size_box, 'tr_TP': tr_TPs, 'tr_TN': tr_TNs, 'tr_FP': tr_FPs, 'tr_FN': tr_FNs, 'val_TP': val_TPs, 'val_TN': val_TNs, 'val_FP': val_FPs, 'val_FN': val_FNs, 'ts_TP': ts_TPs, 'ts_TN': ts_TNs, 'ts_FP': ts_FPs, 'ts_FN': ts_FNs})
+    df2 = df(data={'index': index, 'nodes': nodes_box, 'learning_rate': learning_rate_box, 'batch_sizes': batch_size_box, 'tr_TP': tr_TPs, 'tr_TN': tr_TNs, 'tr_FP': tr_FPs, 'tr_FN': tr_FNs, 'ts_TP': ts_TPs, 'ts_TN': ts_TNs, 'ts_FP': ts_FPs, 'ts_FN': ts_FNs})
     df2.to_csv("../result/OV_DNN_result_"+file_type+"_raw.csv", index=False, header=True, mode='w')
-
-    
 
 
 len(test_h)
